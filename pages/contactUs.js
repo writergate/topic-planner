bebfa66ea9d3eefc866ee5171ba282ea98d3c922
Anuja-2000 <anujawij@gmail.com>
2023-02-08 01:00:37 +0530

    center the component

--- pages/contactUs.js
+++ pages/contactUs.js
@@ -1,11 +1,21 @@
-import { Paper, Grid, Typography, Button, TextField } from '@mui/material';
+import { Paper, Grid, Typography, Button, TextField} from '@mui/material';
 import Container from '@mui/material/Container'
+import { useState } from 'react';
 
 export default function ContactUs() {
-  return (
+  const [name,setName] = useState('')
+  const [nameError,setNameError] = useState(false)
 
-    <Container>
-      <Paper elevation={4}>
+  const validateForm = (e) => {
+    e.preventDefault()
+    if(name == ''){
+      setNameError(true)
+    }
+    console.log(name)
+  }
+  return (
+<Container maxWidth="xl">
+      <Paper elevation={4} >
         <Grid container spacing={2}>
           <Grid item xs = {12}>
             <Typography variant="h4" color="primary" align='center'>Contact us</Typography>
@@ -15,14 +25,17 @@ export default function ContactUs() {
           <Typography variant="subtitle1" color="black" align='center'>Image by <a href="https://www.freepik.com/free-vector/flat-design-illustration-customer-support_12982910.htm#query=contact%20us&position=0&from_view=search&track=sph">Freepik</a></Typography>
           </Grid>
           <Grid item xs ={6}>
+            <form onSubmit={validateForm()}>
             <TextField
+              onChange={(e)=>setName(e.target.value)}
               id="name"
               label="Name"
               type="text"
               variant="outlined"
               margin='normal'
               required='true'
-              
+              sx={{width:'70%'}}
+              error={nameError}
             />
             <br/>
             <TextField
@@ -30,24 +43,29 @@ export default function ContactUs() {
               label="Email"
               type="email"
               variant="outlined"
+              required='true'
               margin='normal'
+              sx={{width:'70%'}}
             />
             <br/>
             <TextField
               id="message"
               label="Contact message"
               variant="outlined"
+              required='true'
               margin='normal'
               multiline='true'
               rows='10'
+              sx={{width:'70%'}}
             />
             <br/>
-            <Button variant="contained" color="primary">
+            <Button variant="contained" color="primary" type='submit' sx={{width:'40%',marginBottom:2, padding:1}} onClick={validateForm()}>
              Submit 
             </Button>
+            </form>
           </Grid>
         </Grid>
       </Paper>
-    </Container>
+    </Container>  
   );
 }
