c1236f803626218991f83e3aa3027ab2b18fae2c
shiran-dinushka <shirandinushka9@gmail.com>
2023-03-23 23:20:35 +0530

    add navbar to search page

--- pages/search.js
+++ pages/search.js
@@ -6,6 +6,9 @@ import {TextField, IconButton, InputAdornment} from '@mui/material';
 import styled from 'styled-components';
 import SearchIcon from '@mui/icons-material/Search';
 import SearchArticleBox from './searchArticle/searchArticleBox';
+import { AppBar, Toolbar, Typography } from '@mui/material';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 
 const SearchTextField = styled(TextField)({
@@ -35,10 +38,37 @@ function Search(){
                 <link rel="icon" href="/favicon.ico"/> 
             </Head>
             <div className={style.navbar}>
-                
+            <AppBar bposition="fixed"sx={{backgroundColor: '#0082e6'}}>
+                <Toolbar>
+                    <Typography variant="h6"  >
+                        (our logo)
+                    </Typography>
+                    <div style={{ flexGrow: 1 }}></div>
+                    <IconButton
+                    size="large"
+                    color="white"
+                    aria-label="search"
+                    >
+                    </IconButton>
+                    <IconButton
+                    size="large"
+                    color="white" // this don't work
+                    aria-label="notifications"
+                    >
+                    <NotificationsIcon />
+                    </IconButton>
+                    <IconButton
+                    size="large"
+                    color="white" // this don't work
+                    aria-label="account"
+                    >
+                    <AccountCircleIcon />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
             </div>
             <div className={style.searchBox}>
-                <Box sx={{ width: 1350, maxWidth: '100%',display:'flex',justifyContent:'center',alignItems:'center'}}>
+                <Box sx={{ width: 1350, maxWidth: '100%'}}>
                     <SearchTextField fullWidth  placeholder="Search Article" id="fullWidth" type="search" 
                     InputProps={{style: {borderRadius: '20px'} ,  startAdornment: (
                         <InputAdornment position="start">
