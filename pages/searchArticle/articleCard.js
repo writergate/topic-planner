203d3802c2294092c0650344979da8609892348c
shiran-dinushka <shirandinushka9@gmail.com>
2023-03-24 15:05:02 +0530

    design article cards

--- pages/searchArticle/articleCard.js
+++ pages/searchArticle/articleCard.js
@@ -4,33 +4,29 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
+import Avatar from '@mui/material/Avatar';
 
 function ArticleCard() {
   return (
-    <Card sx={{ maxWidth: 320 }}>
+    <Card sx={{ width: 320,height:350,borderRadius: 4, backgroundColor: '#eee',boxShadow:4 }}>
       <CardActionArea>
         <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" 
+          sx={{ width: 25, height: 25, marginTop:1,marginBottom:2}}/>
+          <Typography gutterBottom variant="h5" component="div" 
+          style={{color: '#111', fontWeight:600,height:70,fontSize:20}}>
+            CSS Nesting is now available with Vanilla CSS
           </Typography>
         </CardContent>
         <CardMedia
           component="img"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
+          height="170"
+          width="300"
+          image="css.jpg"
           alt="green iguana"
+          style={{paddingRight:10,paddingLeft:10,borderRadius:22}}
         />
-        
       </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-      </CardActions>
     </Card>
   );
 }
