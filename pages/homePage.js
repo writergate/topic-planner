75f1d7df28b322770c3a6f9bd79832d3e4b11f26
shiran-dinushka <shirandinushka9@gmail.com>
2023-03-27 08:26:45 +0530

    add navigation bar to home page

--- pages/homePage.js
+++ pages/homePage.js
@@ -1,12 +1,19 @@
 import TrendingArticleBox from "./Trending/trendingArticleBox";
 import Styles from '../styles/homePage.module.css'
- 
+import React from 'react';
+import { Button } from "@mui/material";
+
 
 function HomePage(){
 
     return(
         <div className={Styles.outer}>
-            <div className={Styles.navBar}></div>
+            <div className={Styles.navBar} >
+                <div style={{ flexGrow: 1 }} ></div>
+                <Button href="#text-buttons" sx={{marginRight:4}}>Contact Us</Button>
+                <Button href="#text-buttons" sx={{marginRight:4}}>Sign In</Button>
+                <Button sx={{borderRadius:6,marginRight:14}} variant="contained">Get Started</Button>
+            </div>
             <div className={Styles.introBox}></div>
             <div className={Styles.trends}>< TrendingArticleBox /> </div>
         </div>
